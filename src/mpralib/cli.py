10ad8e6730232350479d9f40c2a3f8d132be8a1e
Max Schubach <max.schubach@bih-charite.de>
2025-03-18 15:12:16 +0100

    Implement enhancements to improve script performance and usability

--- src/mpralib/cli.py
+++ src/mpralib/cli.py
@@ -447,10 +447,10 @@ def get_reporter_elements(
     out_df.loc[:, "inputCount"] = mpradata.normalized_dna_counts.mean(axis=0)
     out_df.loc[:, "outputCount"] = mpradata.normalized_rna_counts.mean(axis=0)
     out_df.dropna(inplace=True)
-    out_df.loc[:, "minusLog10PValue"] = -np.log10(out_df.loc[:, "P.Value"])
-    out_df.loc[:, "minusLog10QValue"] = -np.log10(out_df.loc[:, "adj.P.Val"])
-    out_df["minusLog10PValue"] = out_df["minusLog10PValue"].apply(lambda x: 0 if x == -0 else x)
-    out_df["minusLog10QValue"] = out_df["minusLog10QValue"].apply(lambda x: 0 if x == -0 else x)
+    out_df.loc[out_df["P.Value"] == 0, "P.Value"] = np.finfo(float).eps
+    out_df.loc[out_df["adj.P.Val"] == 0, "adj.P.Val"] = np.finfo(float).eps
+    out_df.loc[:, "minusLog10PValue"] = np.abs(np.log10(out_df.loc[:, "P.Value"]))
+    out_df.loc[:, "minusLog10QValue"] = np.abs(np.log10(out_df.loc[:, "adj.P.Val"]))
     out_df = out_df.rename(columns={"oligo": "oligo_name", "logFC": "log2FoldChange"})
     out_df[
         [
@@ -461,7 +461,7 @@ def get_reporter_elements(
             "minusLog10PValue",
             "minusLog10QValue",
         ]
-    ].to_csv(output_reporter_elements_file, sep="\t", index=False, float_format='%.4f')
+    ].to_csv(output_reporter_elements_file, sep="\t", index=False, float_format="%.4f")
 
 
 @cli.command()
@@ -503,7 +503,226 @@ def get_reporter_elements(
 )
 def get_reporter_variants(input_file, metadata_file, statistics_file, bc_threshold, output_reporter_variants_file):
 
-    # TODO: Fix method
+    mpradata = MPRABarcodeData.from_file(input_file)
+
+    mpradata.add_metadata_file(metadata_file)
+
+    mpradata.barcode_threshold = bc_threshold
+
+    mask = mpradata.data.var["category"] == "variant"
+    mpradata.var_filter = mpradata.var_filter | ~np.repeat(np.array(mask)[:, np.newaxis], 3, axis=1)
+
+    mpradata = mpradata.oligo_data
+
+    variant_map = mpradata.variant_map
+
+    df = pd.read_csv(statistics_file, sep="\t", header=0, index_col=0)
+
+    dna_counts = mpradata.dna_counts.copy()
+    rna_counts = mpradata.rna_counts.copy()
+
+    rna_counts_sum = rna_counts.sum(axis=1)
+    dna_counts_sum = dna_counts.sum(axis=1)
+
+    for spdi, row in variant_map.iterrows():
+
+        mask_ref = mpradata.oligos.isin(row["REF"])
+        mask_alt = mpradata.oligos.isin(row["ALT"])
+
+        dna_counts_ref = dna_counts[:, mask_ref].sum(axis=1)
+        rna_counts_ref = rna_counts[:, mask_ref].sum(axis=1)
+
+        dna_counts_alt = dna_counts[:, mask_alt].sum(axis=1)
+        rna_counts_alt = rna_counts[:, mask_alt].sum(axis=1)
+
+        if spdi in df.index:
+            df.loc[spdi, "inputCountRef"] = ((dna_counts_ref / dna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "inputCountAlt"] = ((dna_counts_alt / dna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "outputCountRef"] = ((rna_counts_ref / rna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "outputCountAlt"] = ((rna_counts_alt / rna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "variantPos"] = int(mpradata.data.var["variant_pos"][mpradata.oligos.isin(row["REF"])].values[0][0])
+
+    df.loc[df["P.Value"] == 0, "P.Value"] = np.finfo(float).eps
+    df.loc[df["adj.P.Val"] == 0, "adj.P.Val"] = np.finfo(float).eps
+    df["minusLog10PValue"] = np.abs(np.log10(df["P.Value"]))
+    df["minusLog10QValue"] = np.abs(np.log10(df["adj.P.Val"]))
+    df.rename(
+        columns={
+            "CI.L": "CI_lower_95",
+            "CI.R": "CI_upper_95",
+            "logFC": "log2FoldChange",
+        },
+        inplace=True,
+    )
+    df["postProbEffect"] = df["B"].apply(lambda x: math.exp(x) / (1 + math.exp(x)))
+    df["variant_id"] = df.index
+    df["refAllele"] = df["variant_id"].apply(lambda x: x.split(":")[2])
+    df["altAllele"] = df["variant_id"].apply(lambda x: x.split(":")[3])
+    df["variantPos"] = df["variantPos"].astype(int)
+
+    df[
+        [
+            "variant_id",
+            "log2FoldChange",
+            "inputCountRef",
+            "outputCountRef",
+            "inputCountAlt",
+            "outputCountAlt",
+            "minusLog10PValue",
+            "minusLog10QValue",
+            "postProbEffect",
+            "CI_lower_95",
+            "CI_upper_95",
+            "variantPos",
+            "refAllele",
+            "altAllele",
+        ]
+    ].to_csv(output_reporter_variants_file, sep="\t", index=False, float_format="%.4f")
+
+
+@cli.command()
+@click.option(
+    "--input",
+    "input_file",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+    help="Input file path of MPRA results.",
+)
+@click.option(
+    "--metadata",
+    "metadata_file",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+    help="Input file path of MPRA results.",
+)
+@click.option(
+    "--statistics",
+    "statistics_file",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+    help="Input file path of MPRA lm file.",
+)
+@click.option(
+    "--bc-threshold",
+    "bc_threshold",
+    required=False,
+    default=1,
+    type=int,
+    help="Using a barcode threshold for output.",
+)
+@click.option(
+    "--output-reporter-genomic-elements",
+    "output_reporter_genomic_elements_file",
+    required=True,
+    type=click.Path(writable=True),
+    help="Output file of MPRA data object.",
+)
+def get_reporter_genomic_elements(
+    input_file, metadata_file, statistics_file, bc_threshold, output_reporter_genomic_elements_file
+):
+
+    mpradata = MPRABarcodeData.from_file(input_file)
+
+    mpradata.add_metadata_file(metadata_file)
+
+    mpradata.barcode_threshold = bc_threshold
+
+    mpradata = mpradata.oligo_data
+
+    mask = mpradata.data.var["allele"].apply(lambda x: "ref" in x).values | (mpradata.data.var["category"] == "element")
+    mpradata.var_filter = mpradata.var_filter | ~np.repeat(np.array(mask)[:, np.newaxis], 3, axis=1)
+
+    df = pd.read_csv(statistics_file, sep="\t", header=0)
+
+    indexes_in_order = [mpradata.oligos[mpradata.oligos == ID].index.tolist() for ID in df["ID"]]
+    indexes_in_order = [index for sublist in indexes_in_order for index in sublist]
+    df.index = indexes_in_order
+
+    df = df.join(mpradata.oligos, how="right")
+
+    mpradata.data.varm["mpralm_element"] = df
+
+    out_df = mpradata.data.varm["mpralm_element"][["oligo", "logFC", "P.Value", "adj.P.Val"]]
+
+    out_df.loc[:, ["inputCount"]] = mpradata.normalized_dna_counts.mean(axis=0)
+    out_df.loc[:, ["outputCount"]] = mpradata.normalized_rna_counts.mean(axis=0)
+
+    out_df["chr"] = mpradata.data.var["chr"]
+    out_df["start"] = mpradata.data.var["start"]
+    out_df["end"] = mpradata.data.var["end"]
+    out_df["strand"] = mpradata.data.var["strand"]
+
+    # apply ref and element mask
+    out_df = out_df[mask]
+
+    out_df.dropna(inplace=True)
+    scaler = MinMaxScaler(feature_range=(0, 1000))
+    out_df["score"] = scaler.fit_transform(out_df[["logFC"]]).astype(int)
+    out_df.loc[out_df["P.Value"] == 0, "P.Value"] = np.finfo(float).eps
+    out_df.loc[out_df["adj.P.Val"] == 0, "adj.P.Val"] = np.finfo(float).eps
+    out_df["minusLog10PValue"] = np.abs(np.log10(out_df["P.Value"]))
+    out_df["minusLog10QValue"] = np.abs(np.log10(out_df["adj.P.Val"]))
+    out_df.rename(columns={"oligo": "name", "logFC": "log2FoldChange"}, inplace=True)
+    out_df = out_df[
+        [
+            "chr",
+            "start",
+            "end",
+            "name",
+            "score",
+            "strand",
+            "log2FoldChange",
+            "inputCount",
+            "outputCount",
+            "minusLog10PValue",
+            "minusLog10QValue",
+        ]
+    ].sort_values(by=["chr", "start", "end"])
+
+    with pysam.BGZFile(output_reporter_genomic_elements_file, "ab") as f:
+        f.write(out_df.to_csv(sep="\t", index=False, header=False, float_format="%.4f").encode())
+
+
+@cli.command()
+@click.option(
+    "--input",
+    "input_file",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+    help="Input file path of MPRA results.",
+)
+@click.option(
+    "--metadata",
+    "metadata_file",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+    help="Input file path of MPRA results.",
+)
+@click.option(
+    "--statistics",
+    "statistics_file",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+    help="Input file of the result from mpralm or BCalm.",
+)
+@click.option(
+    "--bc-threshold",
+    "bc_threshold",
+    required=False,
+    default=1,
+    type=int,
+    help="Using a barcode threshold for output.",
+)
+@click.option(
+    "--output-reporter-genomic-variants",
+    "output_reporter_genomic_variants_file",
+    required=True,
+    type=click.Path(writable=True),
+    help="Output file of MPRA data object.",
+)
+def get_reporter_genomic_variants(
+    input_file, metadata_file, statistics_file, bc_threshold, output_reporter_genomic_variants_file
+):
 
     mpradata = MPRABarcodeData.from_file(input_file)
 
@@ -538,230 +757,18 @@ def get_reporter_variants(input_file, metadata_file, statistics_file, bc_thresho
         rna_counts_alt = rna_counts[:, mask_alt].sum(axis=1)
 
         if spdi in df.index:
-            df.loc[spdi, "inputCountRef"] = (
-                (dna_counts_ref / dna_counts_sum) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "inputCountAlt"] = (
-                (dna_counts_alt / dna_counts_sum) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "outputCountRef"] = (
-                (rna_counts_ref / rna_counts_sum) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "outputCountAlt"] = (
-                (rna_counts_alt / rna_counts_sum) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "variantPos"] = int(
-                mpradata.data.var["variant_pos"][mpradata.oligos.isin(row["REF"])].values[0][0]
-            )
-
-    df["minusLog10PValue"] = -np.log10(df["P.Value"])
-    df["minusLog10QValue"] = -np.log10(df["adj.P.Val"])
-    df.rename(
-        columns={
-            "CI.L": "CI_lower_95",
-            "CI.R": "CI_upper_95",
-            "logFC": "log2FoldChange",
-        },
-        inplace=True,
-    )
-    df["postProbEffect"] = df["B"].apply(lambda x: math.exp(x) / (1 + math.exp(x)))
-    df["variant_id"] = df.index
-    df["refAllele"] = df["variant_id"].apply(lambda x: x.split(":")[2])
-    df["altAllele"] = df["variant_id"].apply(lambda x: x.split(":")[3])
-    df["variantPos"] = df["variantPos"].astype(int)
-
-    df[
-        [
-            "variant_id",
-            "log2FoldChange",
-            "inputCountRef",
-            "outputCountRef",
-            "inputCountAlt",
-            "outputCountAlt",
-            "minusLog10PValue",
-            "minusLog10QValue",
-            "postProbEffect",
-            "CI_lower_95",
-            "CI_upper_95",
-            "variantPos",
-            "refAllele",
-            "altAllele",
-        ]
-    ].to_csv(output_reporter_variants_file, sep="\t", index=False, float_format='%.4f')
-
-
-@cli.command()
-@click.option(
-    "--input",
-    "input_file",
-    required=True,
-    type=click.Path(exists=True, readable=True),
-    help="Input file path of MPRA results.",
-)
-@click.option(
-    "--metadata",
-    "metadata_file",
-    required=True,
-    type=click.Path(exists=True, readable=True),
-    help="Input file path of MPRA results.",
-)
-@click.option(
-    "--mpralm",
-    "mpralm_file",
-    required=True,
-    type=click.Path(exists=True, readable=True),
-    help="Input file path of MPRA lm file.",
-)
-@click.option(
-    "--bc-threshold",
-    "bc_threshold",
-    required=False,
-    default=1,
-    type=int,
-    help="Using a barcode threshold for output.",
-)
-@click.option(
-    "--output-reporter-genomic-elements",
-    "output_reporter_genomic_elements_file",
-    required=True,
-    type=click.Path(writable=True),
-    help="Output file of MPRA data object.",
-)
-def get_reporter_genomic_elements(input_file, metadata_file, mpralm_file, bc_threshold, output_reporter_genomic_elements_file):
-
-    mpradata = MPRABarcodeData.from_file(input_file)
-
-    mpradata.add_metadata_file(metadata_file)
-
-    mpradata.barcode_threshold = bc_threshold
-
-    mpradata = mpradata.oligo_data
-
-    mask = mpradata.data.var["allele"].apply(lambda x: "ref" in x).values | (mpradata.data.var["category"] == "element")
-
-    mpradata.var_filter = mpradata.var_filter | ~np.repeat(np.array(mask)[:, np.newaxis], 3, axis=1)
-
-    df = pd.read_csv(mpralm_file, sep="\t", header=0, compression=None)
-
-    indexes_in_order = [mpradata.oligos[mpradata.oligos == ID].index.tolist() for ID in df["ID"]]
-    indexes_in_order = [index for sublist in indexes_in_order for index in sublist]
-    df.index = indexes_in_order
-
-    df = df.join(mpradata.oligos, how="right")
-
-    mpradata.data.varm["mpralm_element"] = df
-
-    out_df = mpradata.data.varm["mpralm_element"][["oligo", "logFC", "P.Value", "adj.P.Val"]]
-    out_df.loc[:, ["inputCount"]] = mpradata.normalized_dna_counts.mean(axis=0)
-    out_df.loc[:, ["outputCount"]] = mpradata.normalized_rna_counts.mean(axis=0)
-
-    out_df["chr"] = mpradata.data.var["chr"]
-    out_df["start"] = mpradata.data.var["start"]
-    out_df["end"] = mpradata.data.var["end"]
-    out_df["strand"] = mpradata.data.var["strand"]
-    out_df.dropna(inplace=True)
-    scaler = MinMaxScaler(feature_range=(0, 1000))
-    out_df["score"] = scaler.fit_transform(out_df[["logFC"]]).astype(int)
-    out_df["minusLog10PValue"] = -np.log10(out_df["P.Value"])
-    out_df["minusLog10QValue"] = -np.log10(out_df["adj.P.Val"])
-    out_df.rename(columns={"oligo": "name", "logFC": "log2FoldChange"}, inplace=True)
-    out_df = out_df[
-        [
-            "chr",
-            "start",
-            "end",
-            "name",
-            "score",
-            "strand",
-            "log2FoldChange",
-            "inputCount",
-            "outputCount",
-            "minusLog10PValue",
-            "minusLog10QValue",
-        ]
-    ].sort_values(by=["chr", "start", "end"])
-
-    with pysam.BGZFile(output_reporter_genomic_elements_file, "ab") as f:
-        f.write(out_df.to_csv(sep="\t", index=False, header=False).encode())
-
-
-@cli.command()
-@click.option(
-    "--input",
-    "input_file",
-    required=True,
-    type=click.Path(exists=True, readable=True),
-    help="Input file path of MPRA results.",
-)
-@click.option(
-    "--metadata",
-    "metadata_file",
-    required=True,
-    type=click.Path(exists=True, readable=True),
-    help="Input file path of MPRA results.",
-)
-@click.option(
-    "--mpralm",
-    "mpralm_file",
-    required=True,
-    type=click.Path(exists=True, readable=True),
-    help="Input file path of MPRA lm file.",
-)
-@click.option(
-    "--output-reporter-genomic-variants",
-    "output_reporter_genomic_variants_file",
-    required=True,
-    type=click.Path(writable=True),
-    help="Output file of MPRA data object.",
-)
-def get_reporter_genomic_variants(input_file, metadata_file, mpralm_file, output_reporter_genomic_variants_file):
-
-    mpradata = MPRABarcodeData.from_file(input_file)
-
-    mpradata.add_metadata_file(metadata_file)
-
-    mpradata.barcode_threshold = 10
-
-    mpradata.apply_barcode_filter(BarcodeFilter.RNA_ZSCORE, {"times_zscore": 3})
-
-    spdi_map = mpradata.variant_map
-
-    df = pd.read_csv(mpralm_file, sep="\t", header=0, index_col=0)
-
-    variant_dna_counts = mpradata.variant_dna_counts
-    variant_rna_counts = mpradata.variant_rna_counts
-
-    columns_ref = []
-    columns_alt = []
-    for replicate in mpradata.oligo_data.obs_names:
-        columns_ref.append("counts_" + replicate + "_REF")
-        columns_alt.append("counts_" + replicate + "_ALT")
-
-    dna_counts = np.array(variant_dna_counts[columns_ref].sum()) + np.array(variant_dna_counts[columns_alt].sum())
-    rna_counts = np.array(variant_rna_counts[columns_ref].sum()) + np.array(variant_rna_counts[columns_alt].sum())
-
-    for spdi, row in spdi_map.iterrows():
-        if spdi in df.index:
-            df.loc[spdi, "inputCountRef"] = (
-                (variant_dna_counts.loc[spdi][columns_ref] / dna_counts) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "inputCountAlt"] = (
-                (variant_dna_counts.loc[spdi][columns_alt] / dna_counts) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "outputCountRef"] = (
-                (variant_rna_counts.loc[spdi][columns_ref] / rna_counts) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "outputCountAlt"] = (
-                (variant_rna_counts.loc[spdi][columns_alt] / rna_counts) * MPRABarcodeData.SCALING
-            ).mean()
-            df.loc[spdi, "variantPos"] = int(
-                mpradata.oligo_data.var["variant_pos"][mpradata.oligos.isin(row["REF"])].values[0][0]
-            )
-            df.loc[spdi, "strand"] = mpradata.oligo_data.var["strand"][mpradata.oligos.isin(row["REF"])].values[0]
+            df.loc[spdi, "inputCountRef"] = ((dna_counts_ref / dna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "inputCountAlt"] = ((dna_counts_alt / dna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "outputCountRef"] = ((rna_counts_ref / rna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "outputCountAlt"] = ((rna_counts_alt / rna_counts_sum) * MPRABarcodeData.SCALING).mean()
+            df.loc[spdi, "variantPos"] = int(mpradata.data.var["variant_pos"][mpradata.oligos.isin(row["REF"])].values[0][0])
+            df.loc[spdi, "strand"] = mpradata.data.var["strand"][mpradata.oligos.isin(row["REF"])].values[0]
 
     df["variantPos"] = df["variantPos"].astype(int)
-    df["minusLog10PValue"] = -np.log10(df["P.Value"])
-    df["minusLog10QValue"] = -np.log10(df["adj.P.Val"])
+    df.loc[df["P.Value"] == 0, "P.Value"] = np.finfo(float).eps
+    df.loc[df["adj.P.Val"] == 0, "adj.P.Val"] = np.finfo(float).eps
+    df["minusLog10PValue"] = np.abs(np.log10(df["P.Value"]))
+    df["minusLog10QValue"] = np.abs(np.log10(df["adj.P.Val"]))
     df.rename(
         columns={
             "CI.L": "CI_lower_95",
@@ -810,7 +817,7 @@ def get_reporter_genomic_variants(input_file, metadata_file, mpralm_file, output
     ].sort_values(by=["chr", "start", "end"])
 
     with pysam.BGZFile(output_reporter_genomic_variants_file, "ab") as f:
-        f.write(df.to_csv(sep="\t", index=False, header=False).encode())
+        f.write(df.to_csv(sep="\t", index=False, header=False, float_format="%.4f").encode())
 
 
 if __name__ == "__main__":
