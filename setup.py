0b6108a89608e31fc8d4cfdc2fcecf08119b5113
Max <visze@users.noreply.github.com>
2025-06-04 13:28:10 +0200

    chore!: MPRAlib is now always in lowercase (also on pypi)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MPRAlib',
+    name='mpralib',
     version='0.6.5',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
